2eacd79afb6cb8d6dcc6cd643922ef91a287b8a7
noam meir <noamariel8@gmail.com>
2022-08-14 12:34:26 +0300

    make it functional

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import './App.css';
-import Option from './Components/Option.js';
+import List from './Components/List.js';
+import {useState } from 'react';
 
 function App() {
+  const [list, setList] = useState({'Milk':{ price: 19.90, quantity:12, icon: "https://cdn-icons-png.flaticon.com/512/3753/3753934.png"},
+                                    'Coffee':{price:12.90, quantity: 2, icon:"https://cdn-icons-png.flaticon.com/512/4856/4856718.png" },
+                                    'Bread':{ price:14.90, quantity: 4, icon: "https://cdn-icons-png.flaticon.com/512/4241/4241664.png"},
+                                    'Tomatto':{ price:2.90, quantity: 7, icon: "https://cdn-icons-png.flaticon.com/512/2909/2909894.png"},
+                                    'Butter':{price:10.00, quantity: 22, icon: "https://cdn2.iconfinder.com/data/icons/food-drink-60/50/1F9C8-butter-512.png"},
+                                    'Meat':{price:16.90, quantity: 1, icon: "https://cdn-icons-png.flaticon.com/512/1046/1046769.png"}})
+  const [cart, setCart] = useState({})
+  const [outOfStock, setOutOfStock] = useState({})
+  const [total, setTotal] = useState(0)
+  function updateFromList(name){
+    console.log(name);
+    cart[name]? cart[name].quantity += 1: cart[name] = {price: list[name].price, quantity: 1, icon: list[name].icon}
+    if (list[name].quantity-1 === 0){
+      outOfStock[name] = {price: list[name].price, quantity: 0, icon: list[name].icon}; 
+      delete list[name]
+    }else{
+      list[name].quantity -= 1
+    }
+    setList({...list})
+    setCart({...cart})
+    setTotal(total+cart[name].price)
+}
+  function updateFromCart(name){
+    console.log(name);
+    list[name]? list[name].quantity += 1: list[name] = {price: cart[name].price, quantity: 1, icon: cart[name].icon}
+    cart[name].quantity-1 === 0? delete cart[name]: cart[name].quantity -= 1
+    if(outOfStock[name]){
+      delete outOfStock[name]
+    }
+    setCart({...cart})
+    setList({...list})
+    setOutOfStock({...outOfStock})
+    setTotal(total-list[name].price)
+}
   return (
     <div className="App">
-      <div className='groccery-list'>
-        <h2>List</h2>
-        <section className='list-content'>
-          <Option name='Tomato' price={19.90} quantity={5}></Option>
-          <label>Tomato</label>
-          <label>Tomato</label>
-        </section>
-      </div>
-      <div className='groccery-list'>
-        <h2>Cart</h2>
-        <section className='list-content'>
-          <label>Tomato</label>
-          <label>Tomato</label>
-        </section>
-        <section className='summary'>
-          <h3>Summary</h3>
-          total: 20$<br></br>
-          <button>Order Now</button>
-        </section>
-      </div>
+      <List list={list} secondList={outOfStock} onClick={(name)=>updateFromList(name)}></List>
+      <List summary={true} total={total} list={cart} onClick={(name)=>updateFromCart(name)}></List>\
     </div>
   );
 }
